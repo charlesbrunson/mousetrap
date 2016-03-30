753ffa616428e75572d6b98bc31e78c6bd2b0847
cbru <charlesobrunson@gmail.com>
2016-03-30 17:08:42 -0500

    another thing

--- app/src/main/java/charles/mobiledevfinalproject/HexGrid.java
+++ app/src/main/java/charles/mobiledevfinalproject/HexGrid.java
@@ -66,10 +66,10 @@ public class HexGrid {
 
                 // 0,0 is the top left most cell
 
-                // 0. (0 )(1 )(2 )(3 )
-                // 1.   (0 )(1 )(2 )(3 )
-                // 2. (0 )(1 )(2 )(3 )
-                // 4.   (0 )(1 )(2 )(3 )
+                // (0,0) (1,0) (2,0) (3,0)
+                //    (0,1) (1,1) (2,1) (3,1)
+                // (0,2) (1,2) (2,2) (3,2)
+                //    (0,3) (1,3) (2,3) (3,3)
 
                 // link Cells to their neighbors
                 // Cells that still have null as one of their neighbors
