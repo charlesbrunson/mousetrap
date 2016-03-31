6cacf7ebbab4a77ac46ab7edcd84d0468a721cf4
cbru <charlesobrunson@gmail.com>
2016-03-31 00:06:07 -0500

    hex grid more or less done

--- app/src/main/java/charles/mobiledevfinalproject/HexGrid.java
+++ app/src/main/java/charles/mobiledevfinalproject/HexGrid.java
@@ -2,32 +2,58 @@ package charles.mobiledevfinalproject;
 
 import android.util.Log;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
+
 // this is the data structure that will manage all information about the game
 // the hex cells, where the mouse is at, moving the mouse, etc
 
 public class HexGrid {
-    public static final int cellWidth = 9;
-    public static final int cellHeight = 9;
+    // grid is actually 9x9 with cells on the edges for mouse to escape to
+    public static final int cellWidth = 11;
+    public static final int cellHeight = 11;
     public static final int startingWalls = 7;
 
-    private class Cell {
-        static final int EMPTY = 0;
-        static final int WALL = 1;
-        static final int MOUSE = 2;
+    public class Cell {
+        private static final int EMPTY = 0;
+        private static final int WALL = 1;
+        private static final int MOUSE = 2;
 
         Cell (int X, int Y) {
             x = X;
             y = Y;
         }
 
-        //position
-        int x = 0;
-        int y = 0;
+        // position
+        private int x = 0;
+        private int y = 0;
 
-        //contents of this cell
+        // contents of this cell
         int contains = EMPTY;
 
-        //references to adjacent cells
+        // -1 means cell is disconnected from edge
+        int distFromEdge = -1;
+
+        //getters
+        public boolean isEmpty() {
+            return contains == EMPTY;
+        }
+        public boolean isWall() {
+            return contains == WALL;
+        }
+        public boolean isMouse() {
+            return contains == MOUSE;
+        }
+        public int getX() {
+            return x;
+        }
+        public int getY() {
+            return y;
+        }
+
+        // references to adjacent cells
         Cell NW = null;
         Cell NE = null;
 
@@ -91,7 +117,6 @@ public class HexGrid {
                         }
                     }
                     if (y < cellHeight - 1) {
-
                         i.SE = cells[x][y + 1];
                         if (x > 0) {
                             i.SW = cells[x - 1][y + 1];
@@ -106,7 +131,6 @@ public class HexGrid {
                         }
                     }
                     if (y < cellHeight - 1) {
-
                         i.SW = cells[x][y + 1];
                         if (x < cellWidth - 1) {
                             i.SE = cells[x + 1][y + 1];
@@ -115,6 +139,8 @@ public class HexGrid {
                 }
             }
         }
+
+        initialize();
     }
 
     // set up a new game
@@ -127,22 +153,57 @@ public class HexGrid {
             }
         }
 
+        Random r = new Random();
+
         // TODO
 
-        // randomly place mouse in/next to center
+        //randomize mouse placement near center
+        int centerX = cellWidth / 2;
+        int centerY = cellHeight / 2;
+
+        Cell mouseCell = null;
+        switch (r.nextInt(7)) {
+            case 0: mouseCell = cells[centerX][centerY]; break;
+            case 1: mouseCell = cells[centerX][centerY].E; break;
+            case 2: mouseCell = cells[centerX][centerY].NE; break;
+            case 3: mouseCell = cells[centerX][centerY].NW; break;
+            case 4: mouseCell = cells[centerX][centerY].SE; break;
+            case 5: mouseCell = cells[centerX][centerY].SW; break;
+            case 6: mouseCell = cells[centerX][centerY].W; break;
+        }
+        if (mouseCell != null)
+            mouseCell.contains = Cell.MOUSE;
 
         // randomly place walls on hex grid
+        int count = 0;
+        while (count < startingWalls) {
+
+            //ignore outer edge
+            int x = 1 + r.nextInt(cellWidth - 2);
+            int y = 1 + r.nextInt(cellHeight - 2);
+
+            Cell c = getCell(x, y);
+            if (c != null && c.isEmpty()) {
+                c.contains = Cell.WALL;
+                count++;
+            }
+        }
 
         // reset moveCounter
         moveCounter = 0;
+        updateCellDistances();
     }
 
     // return true if wall is successfully placed
     // also increments players move counter
     public boolean placeWall(int x, int y) {
 
-        if (cells[x][y].contains == Cell.EMPTY) {
+        if (    x != 0 && x != cellWidth - 1 &&
+                y != 0 && y != cellHeight - 1 &&
+                cells[x][y].isEmpty()) {
+
             cells[x][y].contains = Cell.WALL;
+            updateCellDistances();
             moveCounter++;
             return true;
         }
@@ -150,26 +211,64 @@ public class HexGrid {
     }
 
     // if this returns false, the mouse cannot be moved, player wins
-    public boolean moveMouse () {
+    public Cell moveMouse () {
 
         // TODO
 
-        // try to find shortest path out of grid
+        if (mouseCell == null)
+            return null;
 
-        // if two or more cell are equidistance from out of grid, pick one at random
+        LinkedList<Cell> moveOptions = new LinkedList<Cell>();
+        moveOptions.add(mouseCell.E);
+        moveOptions.add(mouseCell.W);
+        moveOptions.add(mouseCell.NE);
+        moveOptions.add(mouseCell.NW);
+        moveOptions.add(mouseCell.SE);
+        moveOptions.add(mouseCell.SW);
 
-        // if mouse is trapped, move to random empty cell
+        // find adjacent cell with lowest distance
+        int lowestDist = Integer.MAX_VALUE;
+        for (int i = 0; i < moveOptions.size(); i++) {
 
+            Cell c = moveOptions.get(i);
+
+            if (c != null && c.isEmpty() && c.distFromEdge >= 0 && c.distFromEdge < lowestDist) {
+                lowestDist = c.distFromEdge;
+            }
+        }
+
+        //remove non options
+        for (int i = 0; i < moveOptions.size(); i++) {
+            Cell c = moveOptions.get(i);
+            if (    c == null || (!c.isEmpty() ||
+                    (c.distFromEdge >= 0 && c.distFromEdge > lowestDist))) {
+                moveOptions.remove(i);
+                i--;
+            }
+        }
+
+        // if two or more cell are equidistant from out of grid, pick one at random
         // if the mouse is successfully moved return true
+        if (moveOptions.size() > 1) {
+            Random r = new Random();
+            mouseCell.contains = Cell.EMPTY;
+            mouseCell = moveOptions.get(r.nextInt(moveOptions.size()));
+            mouseCell.contains = Cell.MOUSE;
+        }
+        else if (moveOptions.size() == 0) {
+            mouseCell.contains = Cell.EMPTY;
+            mouseCell = moveOptions.get(0);
+            mouseCell.contains = Cell.MOUSE;
+        }
 
-        return false;
+        // return mouse cell
+        // if mouse cell's distance is zero then the mouse has won
+        return mouseCell;
     }
 
     public Cell getCell (int x, int y) {
-        try {
+        if (x >= 0 && x < cellWidth && y >= 0 && y < cellHeight) {
             return cells[x][y];
-        } catch (IndexOutOfBoundsException e) {
-            Log.d("HEXGRID: ", e.getLocalizedMessage());
         }
         return null;
     }
@@ -182,4 +281,86 @@ public class HexGrid {
         return moveCounter;
     }
 
+    // this is used for mouse movement
+    private void updateCellDistances() {
+
+        //use wavefront algorithm to determine distances from edge
+
+        //all the cells that have a distance
+        Stack<Cell> visited = new Stack<>();
+        Queue<Cell> fringe = new LinkedList<>();
+
+        //start with edges
+        for (int x = 0; x < cellWidth; x++) {
+            for (int y = 0; y < cellHeight; y++) {
+                Cell i = cells[x][y];
+
+                if (    i.E  == null || i.W  == null ||
+                        i.NE == null || i.NW == null ||
+                        i.SE == null || i.SW == null) {
+                    //cell is on the edge
+                    i.distFromEdge = 0;
+
+                    fringe.add(i);
+
+                }
+            }
+        }
+
+        while (!fringe.isEmpty()) {
+            Cell c = fringe.peek();
+
+            //search neighbors
+            for (int i = 0; i < 6; i++) {
+                Cell n = null;
+                switch (i) {
+                    case 0: n = c.E; break;
+                    case 1: n = c.SE; break;
+                    case 2: n = c.SW; break;
+                    case 3: n = c.W; break;
+                    case 4: n = c.NW; break;
+                    case 5: n = c.NE; break;
+                }
+
+                if (n != null && !n.isWall() && !visited.contains(n)) {
+
+                    //check if neighbor is in fringe first
+                    if (fringe.contains(n)) {
+
+                        n.distFromEdge = Math.min(n.distFromEdge, c.distFromEdge + 1);
+
+                    }
+                    //else it's a new node add to fringe
+                    else {
+
+                        n.distFromEdge = c.distFromEdge + 1;
+                        fringe.add(n);
+
+                    }
+
+                }
+            }
+
+            //all neighbors evaluated, add to visited, remove from fringe
+            visited.push(fringe.remove());
+        }
+
+        //testing
+        for (int y = 0; y < cellHeight; y++) {
+            StringBuilder debug = new StringBuilder();
+
+            if (y % 2 == 1)
+                debug.append(" ");
+
+            for (int x = 0; x < cellWidth; x++) {
+                if (getCell(x,y).isEmpty())
+                    debug.append(getCell(x,y).distFromEdge + " ");
+                else if (getCell(x,y).isWall())
+                    debug.append("W ");
+                else if (getCell(x,y).isMouse())
+                    debug.append("M ");
+            }
+            Log.d("HEXGRID", debug.toString());
+        }
+    }
 }
